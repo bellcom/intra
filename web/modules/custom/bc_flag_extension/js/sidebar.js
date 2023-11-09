10b76681d1b81c9e28d59e9e6c4c22789007cadf
Henrik Tholle <ht@bellcom.dk>
2023-11-09 10:51:48 +0100

    RINGSTED-861 updated

--- web/modules/custom/bc_flag_extension/js/sidebar.js
+++ web/modules/custom/bc_flag_extension/js/sidebar.js
@@ -2,10 +2,12 @@
 (function($, Drupal, drupalSettings) {
 
   const updateFlagSidebar = function() {
-    let bookmark = $(".sidebar-block.sidebar-bookmark");
-    if (bookmark.length && bookmark.is(":visible")) {
+
       let nid = 0;
-      if (drupalSettings.data.nid) nid = drupalSettings.data.nid;
+      if (drupalSettings.data.nid) {
+        nid = drupalSettings.data.nid;
+      }
+
       $.ajaxSetup({cache: false});
       $.getJSON('/bc_flag_extension/data', {nid: nid}, function (response) {
 
@@ -14,6 +16,7 @@
         let uiu = $(".sidebar-unread-list-ul").empty();
 
         if (Object.keys(response.bookmarks).length > 0) {
+          $(".sidebar-block.sidebar-bookmark").css("display", "flex");
           $.each(response.bookmarks, function (id, item) {
             uib.append($('<li/>').addClass('bookmark-item').attr("data-id", item.flag)
               .append($('<a/>').addClass("bookmark-item-link").attr("href", item.link).text(item.title))
@@ -22,6 +25,7 @@
         }
 
         if (Object.keys(response.shortcuts).length > 0) {
+          $(".sidebar-block.sidebar-shortcut").css("display", "flex");
           $.each(response.shortcuts, function (id, item) {
             uis.append($('<li/>').addClass('shortcut-item').attr("data-id", item.flag)
               .append($('<a/>').addClass("shortcut-item-link").attr("href", item.link).text(item.title))
@@ -30,6 +34,8 @@
         }
 
         if (Object.keys(response.unreads).length > 0) {
+          $(".sidebar-block.sidebar-unread").css("display", "flex");
+          $(".unread-number").html(Object.keys(response.unreads).length);
           $.each(response.unreads, function (id, item) {
             uiu.append($('<li/>').addClass('unread-item').attr("data-id", item.flag)
               .append($('<a/>').addClass("unread-item-link").attr("href", item.link).text(item.title))
@@ -38,7 +44,7 @@
         }
 
       }).done(function() {  });
-    }
+
   };
 
   updateFlagSidebar();

0d6fbced28683eb4b94acf3c29c1a11415c31582
Henrik Tholle <ht@bellcom.dk>
2023-11-13 08:03:39 +0100

    fix

--- web/themes/custom/subsites/fds_midtpunktet_theme/theme-settings.php
+++ web/themes/custom/subsites/fds_midtpunktet_theme/theme-settings.php
@@ -1,6 +1,6 @@
 <?php
 
-function fds_redwhite_theme_form_system_theme_settings_alter(
+function fds_midtpunktet_theme_form_system_theme_settings_alter(
   &$form,
   Drupal\Core\Form\FormStateInterface $form_state
 ) {

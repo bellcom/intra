b824ba40e07392825e7b68f03e0fffbb30a4f0a0
Henrik Tholle <ht@bellcom.dk>
2023-11-13 09:36:03 +0100

    Fix missing code

---
+++ web/modules/custom/bc_basic/src/Commands/BatchCommands.php
@@ -0,0 +1,24 @@
+<?php
+namespace Drupal\bc_basic\Commands;
+
+use Drush\Commands\DrushCommands;
+Class BatchCommands extends DrushCommands
+{
+
+  /**
+   * List all hooks
+   *
+   * @command bc:hooks
+   * @aliases bch
+   * @options $options arr AN option that takes multiple values.
+   */
+  public function showHooks($options=array())
+  {
+
+
+
+  }
+
+
+
+}

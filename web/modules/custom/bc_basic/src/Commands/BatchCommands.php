5cd44cd503ae0517441bc676bc3c4b4779911301
Henrik Tholle <ht@bellcom.dk>
2023-11-16 15:57:16 +0100

    RINGSTED-846 update unread list when subscribe and unsubscribe

--- web/modules/custom/bc_basic/src/Commands/BatchCommands.php
+++ web/modules/custom/bc_basic/src/Commands/BatchCommands.php
@@ -1,9 +1,24 @@
 <?php
 namespace Drupal\bc_basic\Commands;
 
+use Drupal\views\Views;
 use Drush\Commands\DrushCommands;
 Class BatchCommands extends DrushCommands
 {
+
+  /**
+   * test script, put whatever you want here
+   *
+   * @command bc:test
+   * @aliases bct
+   * @options $options arr AN option that takes multiple values.
+   */
+  public function test($options=array()) {
+
+
+  }
+
+
 
   /**
    * List all hooks
---
+++ web/modules/custom/bc_og_extension/src/Form/OgForm.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\bc_og_extension\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\og\Og;
+
+class OgForm extends ConfigFormBase {
+
+  public static $configName = 'bc_og_extension.settings';
+
+  public function getFormId() {
+    return 'bc_og_extension_settings';
+  }
+
+  protected function getEditableConfigNames() {
+    return [OgForm::$configName];
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $config = $this->config(OgForm::$configName);
+
+    $form['form']['default_active'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Set group membership active by default ( default is pending )'),
+      '#default_value' => $config->get('default_active')
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+
+    $values = $form_state->getValues();
+    $config = $this->config(OgForm::$configName);
+    foreach ($values as $key => $value) {
+      $config->set($key, $value);
+    }
+    $config->save();
+
+    parent::submitForm($form, $form_state);
+
+  }
+
+
+}

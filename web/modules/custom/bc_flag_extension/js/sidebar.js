41259ab4a88219894a2343f72d2c56b18d3ef0d7
Henrik Tholle <ht@bellcom.dk>
2023-11-22 13:27:18 +0000

    RINGSTED-873 add counter to menu

--- web/modules/custom/bc_flag_extension/js/sidebar.js
+++ web/modules/custom/bc_flag_extension/js/sidebar.js
@@ -37,13 +37,30 @@
           $(".sidebar-block.sidebar-unread").css("display", "flex");
           $(".unread-number").html(Object.keys(response.unreads).length);
           $.each(response.unreads, function (id, item) {
+
             uiu.append($('<li/>').addClass('unread-item').attr("data-id", item.flag)
               .append($('<a/>').addClass("unread-item-link").attr("href", item.link).text(item.title))
             );
+
+
+
           });
         }
 
-      }).done(function() {  });
+
+
+      }).done(function(response) {
+
+        if ( typeof(response.unreads_count) !== "undefined") {
+          if (typeof(response.unreads_count.og_group_content) !== "undefined") {
+            let unreads = response.unreads_count.og_group_content.count;
+            let menuitem = $('.menu-item *[data-drupal-link-system-path="node/12"]').first();
+            if (unreads && menuitem.length == 1) {
+              menuitem.text( menuitem.text() + ' (' + unreads + ')');
+            }
+          }
+        }
+      });
 
   };
 

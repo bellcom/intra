1dd129f651893b1f8114d3a124e179e09c00950f
Angutivik Casper Rúnur Tausen <ch@bellcom.dk>
2023-11-23 14:02:16 +0100

    Add some more logic to the bc_webdav_buttons

---
+++ web/modules/custom/bc_webdav_buttons/src/Controller/BcWebdavButtonsController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\bc_webdav_buttons\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class BcWebdavButtonsController extends ControllerBase {
+
+  /**
+   * Page callback for the WebDav Log.
+   */
+  public function log() {
+    $user = \Drupal::currentUser();
+
+    // Save log to our custom table 'bc_webdav_buttons_log'.
+    $query = \Drupal::database()->insert('bc_webdav_buttons_log')
+      ->fields([
+        'unix_timestamp' => REQUEST_TIME,
+        'nid' => \Drupal::request()->query->get('nid'),
+        'uid' => $user->id(),
+        'fid' => \Drupal::request()->query->get('fid'),
+        'action' => \Drupal::request()->query->get('action'),
+      ])
+      ->execute();
+
+    // Additional logic for the WebDav Log.
+
+    return [
+      '#markup' => $this->t('WebDav Log content goes here.'),
+    ];
+  }
+
+  /**
+   * Other controller methods can go here.
+   */
+
+}

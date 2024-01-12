661daf6578d28740118258160e54816c40fd8d15
Simon L. Lange <sl@bellcom.dk>
2024-01-12 09:28:00 +0000

    revert js

--- web/modules/custom/bc_webdav/js/webdav-styling.js
+++ web/modules/custom/bc_webdav/js/webdav-styling.js
@@ -1,46 +1,19 @@
-console.log('webdav js loaded');
+console.log('loaded');
 
 (function($, Drupal, drupalSettings) {
-  $(document).ready(function() {
-    $(document).on("click", ".webdav-buttons .webdav-button", function(event) {
-      console.log("Button clicked", this);
-      var action = $(this).data('action');
-      var fileId = $(this).data('id');
 
-      if (action === 'history') {
-        event.preventDefault();
-        fetchHistoryData(fileId);
-      } else {
-        $("#webdavframe").attr("src", '/webdavhandler/?action=' + action + '&id=' + fileId);
-      }
-    });
-
-    function fetchHistoryData(fileId) {
-      $.ajax({
-        url: '/webdavhandler/?action=history&id=' + fileId,
-        dataType: 'json',
-        success: function(response) {
-          if (response.success) {
-            showHistoryModal(response.data);
-          }
-        },
-        error: function() {
-          alert('Error fetching history');
-        }
-      });
-    }
-
-    function showHistoryModal(data) {
-      var modalContent = '';
-      data.forEach(function(row) {
-        modalContent += '<p>User: ' + row.user + ', Action: ' + row.action + ', Time: ' + row.time + '</p>';
-      });
-      $('#customHistoryModal .modal-body').html(modalContent);
-      $('#customHistoryModal').show();
-    }
-
-    $('#customHistoryModal .close').on('click', function() {
-      $('#customHistoryModal').hide();
-    });
+  $(".webdav-buttons .webdav-button").on("click", function() {
+    $("#webdavframe").attr("src", '/webdavhandler/?action=' + $(this).data('action') + '&id=' + $(this).data('id'));
   });
+
+
 })(jQuery, Drupal, drupalSettings);
+
+
+function showBcWebdavLogData(json) {
+  if (json.success) {
+    json.data.forEach((row) => {
+      console.log( row );
+    });
+  }
+}

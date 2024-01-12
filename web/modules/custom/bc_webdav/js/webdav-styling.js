f782db19a34085c91e437c3ce5eff6ab57538a75
Simon L. Lange <sl@bellcom.dk>
2024-01-12 10:47:13 +0000

    Ensure drupal behaviour handles jquery properly

--- web/modules/custom/bc_webdav/js/webdav-styling.js
+++ web/modules/custom/bc_webdav/js/webdav-styling.js
@@ -1,35 +1,29 @@
+(function ($, Drupal) {
+  Drupal.behaviors.customWebdavBehavior = {
+    attach: function (context, settings) {
+      $(".webdav-buttons .webdav-button", context).on("click", function() {
+        $("#webdavframe").attr("src", '/webdavhandler/?action=' + $(this).data('action') + '&id=' + $(this).data('id'));
+      });
 
-(function($, Drupal, drupalSettings) {
+      window.showBcWebdavLogData = function(json) {
+        var modalBody = $('#customHistoryModal .modal-body');
 
-  $(".webdav-buttons .webdav-button").on("click", function() {
-    $("#webdavframe").attr("src", '/webdavhandler/?action=' + $(this).data('action') + '&id=' + $(this).data('id'));
-  });
+        if (json.success) {
+          var contentHtml = '';
+          json.data.forEach((row) => {
+            contentHtml += '<div class="history-entry">';
+            contentHtml += '<p>Time: ' + row.time + '</p>';
+            contentHtml += '<p>User: ' + row.user + '</p>';
+            contentHtml += '<p>Action: ' + row.action + '</p>';
+            contentHtml += '</div>';
+          });
 
-
-})(jQuery, Drupal, drupalSettings);
-
-
-function showBcWebdavLogData(json) {
-  var modalBody = $('#customHistoryModal .modal-body');
-
-  if (json.success) {
-    var contentHtml = '';
-    json.data.forEach((row) => {
-      // Construct the HTML content from each row
-      contentHtml += '<div class="history-entry">';
-      contentHtml += '<p>Time: ' + row.time + '</p>'; // Use the 'time' field
-      contentHtml += '<p>User: ' + row.user + '</p>'; // Use the 'user' field
-      contentHtml += '<p>Action: ' + row.action + '</p>'; // Use the 'action' field
-      contentHtml += '</div>';
-    });
-
-    // Set the modal body's content
-    modalBody.html(contentHtml);
-
-    // Open the modal
-    $('#customHistoryModal').show();
-  } else {
-    modalBody.html('<p>No history data available.</p>');
-  }
-}
-
+          modalBody.html(contentHtml);
+          $('#customHistoryModal').show();
+        } else {
+          modalBody.html('<p>No history data available.</p>');
+        }
+      };
+    }
+  };
+})(jQuery, Drupal);

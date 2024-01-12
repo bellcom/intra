bb546d53719cb27a3b26165845b42a41490ff055
Simon L. Lange <sl@bellcom.dk>
2024-01-12 09:20:01 +0000

    More javascript changes

--- web/modules/custom/bc_webdav/js/webdav-styling.js
+++ web/modules/custom/bc_webdav/js/webdav-styling.js
@@ -1,48 +1,43 @@
 (function($, Drupal, drupalSettings) {
-  // Using event delegation to ensure event binding even on dynamically generated elements
-  $(document).on("click", ".webdav-buttons .webdav-button", function(event) {
-    var action = $(this).data('action');
-    var fileId = $(this).data('id');
+  $(document).ready(function() {
+    $(document).on("click", ".webdav-buttons .webdav-button", function(event) {
+      var action = $(this).data('action');
+      var fileId = $(this).data('id');
 
-    console.log('Button clicked, Action: ' + action + ', File ID: ' + fileId);
-
-    if (action === 'history') {
-      event.preventDefault();
-      console.log('Fetching history for File ID: ' + fileId);
-      fetchHistoryData(fileId);
-    } else {
-      $("#webdavframe").attr("src", '/webdavhandler/?action=' + action + '&id=' + fileId);
-    }
-  });
-
-  function fetchHistoryData(fileId) {
-    $.ajax({
-      url: '/webdavhandler/?action=history&id=' + fileId,
-      dataType: 'json',
-      success: function(response) {
-        console.log('History data received:', response);
-        if (response.success) {
-          showHistoryModal(response.data);
-        }
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.error('Error fetching history:', textStatus, errorThrown);
+      if (action === 'history') {
+        event.preventDefault();
+        fetchHistoryData(fileId);
+      } else {
+        $("#webdavframe").attr("src", '/webdavhandler/?action=' + action + '&id=' + fileId);
       }
     });
-  }
 
-  function showHistoryModal(data) {
-    var modalContent = '';
-    data.forEach(function(row) {
-      modalContent += '<p>User: ' + row.user + ', Action: ' + row.action + ', Time: ' + row.time + '</p>';
+    function fetchHistoryData(fileId) {
+      $.ajax({
+        url: '/webdavhandler/?action=history&id=' + fileId,
+        dataType: 'json',
+        success: function(response) {
+          if (response.success) {
+            showHistoryModal(response.data);
+          }
+        },
+        error: function() {
+          alert('Error fetching history');
+        }
+      });
+    }
+
+    function showHistoryModal(data) {
+      var modalContent = '';
+      data.forEach(function(row) {
+        modalContent += '<p>User: ' + row.user + ', Action: ' + row.action + ', Time: ' + row.time + '</p>';
+      });
+      $('#customHistoryModal .modal-body').html(modalContent);
+      $('#customHistoryModal').show();
+    }
+
+    $('#customHistoryModal .close').on('click', function() {
+      $('#customHistoryModal').hide();
     });
-    $('#customHistoryModal .modal-body').html(modalContent);
-    $('#customHistoryModal').show();
-  }
-
-  // Close modal functionality
-  $(document).on('click', '#customHistoryModal .close', function() {
-    $('#customHistoryModal').hide();
   });
-
 })(jQuery, Drupal, drupalSettings);

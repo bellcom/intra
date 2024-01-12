e5999d502fc471037067618744451c0918f9f442
Simon L. Lange <sl@bellcom.dk>
2024-01-12 09:43:58 +0000

    Modal data

--- web/modules/custom/bc_webdav/js/webdav-styling.js
+++ web/modules/custom/bc_webdav/js/webdav-styling.js
@@ -12,8 +12,22 @@ console.log('loaded');
 
 function showBcWebdavLogData(json) {
   if (json.success) {
+    var modalContent = '';
     json.data.forEach((row) => {
-      console.log( row );
+      // Construct the content to be displayed in the modal
+      modalContent += '<p>User: ' + row.user + ', Action: ' + row.action + ', Time: ' + row.time + '</p>';
     });
+
+    // Populate the modal's body with the constructed content
+    $('#customHistoryModal .modal-body').html(modalContent);
+
+    // Show the modal
+    $('#customHistoryModal').show();
   }
 }
+
+// Add event listener for closing the modal
+$('#customHistoryModal .close').on('click', function() {
+  $('#customHistoryModal').hide();
+});
+

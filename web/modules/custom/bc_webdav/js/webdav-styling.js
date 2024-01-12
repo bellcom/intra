b87708e2328fe9c965a3194724a8faa1db1ffabb
Simon L. Lange <sl@bellcom.dk>
2024-01-12 10:55:57 +0000

    Modal overlay click outside and close logic

--- web/modules/custom/bc_webdav/js/webdav-styling.js
+++ web/modules/custom/bc_webdav/js/webdav-styling.js
@@ -5,6 +5,16 @@
         $("#webdavframe").attr("src", '/webdavhandler/?action=' + $(this).data('action') + '&id=' + $(this).data('id'));
       });
 
+      $('#customHistoryModalCloseButton', context).on('click', function () {
+        $('#customHistoryModal').hide();
+      });
+
+      // Close the modal when the overlay is clicked
+      $('#customHistoryModalOverlay', context).on('click', function () {
+        $('#customHistoryModal').hide();
+        $(this).hide(); // Hide the overlay
+      });
+
       window.showBcWebdavLogData = function(json) {
         var modalBody = $('#customHistoryModal .modal-body');
 

4c7c25e1d7b915514d3634928bd734568fe05897
Simon L. Lange <sl@bellcom.dk>
2024-01-12 11:31:09 +0000

    overlay functionality

--- web/modules/custom/bc_webdav/js/webdav-styling.js
+++ web/modules/custom/bc_webdav/js/webdav-styling.js
@@ -1,4 +1,28 @@
 (function ($, Drupal) {
+  // Define the showBcWebdavLogData function first
+  window.showBcWebdavLogData = function (json) {
+    var modalBody = $('#customHistoryModal .modal-body');
+
+    if (json.success) {
+      var filename = json.filename;
+      var contentHtml = '<h2>' + filename + '</h2>'; // Display the filename
+      contentHtml += '<div class="history-entries">';
+      json.data.forEach((row) => {
+        contentHtml += '<div class="history-entry">';
+        contentHtml += '<div class="history-user"><p>' + row.user + '</p></div>';
+        contentHtml += '<div class="history-time"><p>' + row.time + '</p></div>';
+        contentHtml += '<div class="history-action"><p>' + row.action + '</p></div>';
+        contentHtml += '</div>';
+      });
+
+      modalBody.html(contentHtml);
+      $('#customHistoryModal').show();
+    } else {
+      modalBody.html('<p>No history data available.</p>');
+    }
+  };
+
+  // Attach event handlers here
   Drupal.behaviors.customWebdavBehavior = {
     attach: function (context, settings) {
       $(".webdav-buttons .webdav-button", context).on("click", function() {
@@ -7,6 +31,7 @@
 
       $('#customHistoryModalCloseButton', context).on('click', function () {
         $('#customHistoryModal').hide();
+        $('#customHistoryModalOverlay').hide(); // Hide the overlay
       });
 
       // Close the modal when the overlay is clicked
@@ -14,28 +39,6 @@
         $('#customHistoryModal').hide();
         $(this).hide(); // Hide the overlay
       });
-
-      window.showBcWebdavLogData = function(json) {
-        var modalBody = $('#customHistoryModal .modal-body');
-
-        if (json.success) {
-          var filename = json.filename;
-          var contentHtml = '<h2>' + filename + '</h2>'; // Display the filename
-          contentHtml += '<div class="history-entries">';
-          json.data.forEach((row) => {
-            contentHtml += '<div class="history-entry">';
-            contentHtml += '<div class="history-user"><p>' + row.user + '</p></div>';
-            contentHtml += '<div class="history-time"><p>' + row.time + '</p></div>';
-            contentHtml += '<div class="history-action"><p>' + row.action + '</p></div>';
-            contentHtml += '</div>';
-          });
-
-          modalBody.html(contentHtml);
-          $('#customHistoryModal').show();
-        } else {
-          modalBody.html('<p>No history data available.</p>');
-        }
-      };
     }
   };
 })(jQuery, Drupal);

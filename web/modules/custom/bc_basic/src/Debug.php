fea9824d5a73dcfe65f5c5388d497e77e7d16b2b
Henrik Tholle <ht@bellcom.dk>
2024-01-30 21:30:42 +0100

    Update debugger

--- web/modules/custom/bc_basic/src/Debug.php
+++ web/modules/custom/bc_basic/src/Debug.php
@@ -4,11 +4,14 @@ namespace Drupal\bc_basic;
 
 Class Debug {
 
+  private $logpath = '/var/www/logs';
+  private $logfile = 'debug.log';
+
   public function _log($message=null, $keys=false) : void {
-    if (!empty($message) && is_writable('/var/www/logs')) {
-      $log = '/var/www/logs/debug.log';
-      if (is_string($message)) {
-        file_put_contents($log, "#\n" . print_r( $message , true) . "\n", FILE_APPEND);
+    if (!empty($message) && is_writable($this->logpath)) {
+      $log = $this->logpath . '/' . $this->logfile;
+      if (is_string($message) || is_numeric($message)) {
+        file_put_contents( $log, "#\n" . print_r( $message , true) . "\n", FILE_APPEND);
 
       } elseif ((is_array($message) || is_object($message)) && is_bool($keys) && $keys) {
         file_put_contents($log, "#\n" . print_r( array_keys($message) , true) . "\n", FILE_APPEND);
@@ -23,7 +26,16 @@ Class Debug {
     }
   }
 
+  public function _gettype($message=null) : void {
+    if (!empty($message) && is_writable($this->logpath)) {
+      $log = $this->logpath . '/' . $this->logfile;
+      file_put_contents( $log, "#\n" . gettype($message) . "\n", FILE_APPEND);
+    }
+  }
+
 
   public static function log($message=null, $keys=false) : void { (new self)->_log($message, $keys); }
+  public static function gettype($message=null) : void { (new self())->_gettype($message); }
+
 
 }

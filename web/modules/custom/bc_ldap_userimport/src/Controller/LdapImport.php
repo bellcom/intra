18f782b1f34b771b12381ec5224332ed3f312593
Henrik Tholle <ht@bellcom.dk>
2024-03-20 14:59:55 +0000

    From test server

--- web/modules/custom/bc_ldap_userimport/src/Controller/LdapImport.php
+++ web/modules/custom/bc_ldap_userimport/src/Controller/LdapImport.php
@@ -8,7 +8,7 @@ Class LdapImport {
   public function getData() {
 
     $config = (object) \Drupal::config('bc_ldap_userimport.settings')->get();
-    $results = [];
+    $results = array();
     if ($config->enabled) {
 
       $ldapconn = ldap_connect($config->host) or die("Could not connect to LDAP server.");
@@ -17,52 +17,31 @@ Class LdapImport {
 
       $ldapbind = ldap_bind($ldapconn, $config->rdn, $config->pass) or die("Could not bind to ldap");
 
-      $justthese = [
-        'samaccountname',
-        'dn',
-        'name',
-        'mail',
-        'name',
-        'nickname',
-        'displayname',
-        'memberof',
-        'thumbnailphoto'
-      ];
+      $justthese = array('samaccountname', 'dn', 'name', 'mail', 'name', 'nickname', 'displayname', 'memberof', 'thumbnailphoto');
       //   $justthese = array('samaccountname', 'dn', 'thumbnailphoto', 'mail', 'manager', 'name', 'nickname', 'mailnickname', 'memberof', 'displayname');
-      //   $justthese = array('samaccountname', 'dn', 'mail', 'manager', 'name', 'nickname', 'mailnickname', 'memberof', 'displayname');
-      //   $justthese = array('samaccountname', 'dn', 'mail', 'manager', 'name', 'mailnickname','displayname');
-      //   $justthese = array();
       $idx = 1;
       $counter = 0;
       $cookie = '';
-
       do {
-        $result = ldap_search($ldapconn, $config->dn, $config->filter, $justthese, 0, 0, 0,
-          LDAP_DEREF_NEVER,
-          [
-            [
-              'oid' => LDAP_CONTROL_PAGEDRESULTS,
-              'value' => ['size' => 250, 'cookie' => $cookie]
-            ]
-          ]
+        $result = ldap_search($ldapconn, $config->dn, $config->filter, $justthese, 0, 0, 0, LDAP_DEREF_NEVER,
+          [['oid' => LDAP_CONTROL_PAGEDRESULTS, 'value' => ['size' => 250, 'cookie' => $cookie]]]
         );
 
         ldap_parse_result($ldapconn, $result, $errcode, $matcheddn, $errmsg, $referrals, $controls);
         $entries = ldap_get_entries($ldapconn, $result);
 
         foreach ($entries as $entry) {
-          if (is_array($entry)) {
-            $results[] = $entry;
-          }
+		if (is_array($entry) && !empty($entry)) {
+		  	if (!empty($entry['mail'][0])) { $results[] = $entry; }
+          	}
         }
 
-        echo count($entries) . "\n";
+	echo count($entries) . "\n";
+
         $counter += count($entries);
-
         if (isset($controls[LDAP_CONTROL_PAGEDRESULTS]['value']['cookie'])) {
-          $cookie = $controls[LDAP_CONTROL_PAGEDRESULTS]['value']['cookie'];
-        }
-        else {
+		$cookie = $controls[LDAP_CONTROL_PAGEDRESULTS]['value']['cookie'];
+        } else {
           $cookie = '';
         }
 
@@ -79,16 +58,12 @@ Class LdapImport {
     $list = [];
     foreach ($results as $result) {
 
-      if (!isset($result['samaccountname'])) {
-        continue;
-      }
-      if (!isset($result['samaccountname'][0])) {
-        continue;
-      }
-
-      $entry = new \stdClass();
+      if (!isset($result['samaccountname'])) continue;
+      if (!isset($result['samaccountname'][0])) continue;
+     
       foreach ($result as $key => $value) {
 
+	$entry = new \stdClass();
         if (isset($value['count']) && $value['count'] == 1) {
 
           $key = utf8_encode($key);
